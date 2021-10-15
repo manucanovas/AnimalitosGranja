74b85b461c3961f1400b2e3a76c54db692c14332
andresbustos16 <andresbustos16@gmail.com>
2021-10-15 11:47:15 -0300

    Segundo Commit - Nuevos Atributos Clase Conejo

--- src/EntidadesAnimales/Conejo.java
+++ src/EntidadesAnimales/Conejo.java
@@ -6,4 +6,19 @@ package EntidadesAnimales;
  */
 public class Conejo {
     
+    private int algo;
+    private String nombre;
+    private int cantComida;
+
+    public Conejo() {
+    }
+
+    public Conejo(int algo, String nombre, int cantComida) {
+        this.algo = algo;
+        this.nombre = nombre;
+        this.cantComida = cantComida;
+    }
+    
+    
+    
 }
